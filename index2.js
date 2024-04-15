7a1dc01d47c86bb25b3b804450260dd4cea0125b
azaespa <pauljamesazanes@gmail.com>
2024-04-15 10:24:02 +0800

    build hero

--- index2.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector(".header");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 99) {
-        header.classList.add("on-scroll");
-    } else {
-        header.classList.remove("on-scroll");
-    }
-})
